be82bc7b238cc0306c66bcc236104e274dbd1ad3
boytjuge <ponlawat007@gmail.com>
2023-02-02 23:21:37 +0700

    update class login and register

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify , redirect, render_template , url_for, session,request
 from flask_cors import CORS
-from function.func import *
+from function import func 
 app = Flask(__name__)
 app.secret_key = "@38d8@884*$*8428fdsdf__0fs99"
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -9,7 +9,7 @@ CORS(app)
 
 @app.route("/api/v1/users",methods=['GET'])
 def list_users():
-    data = fn([{'username':'admin'},{'username':'local'}])
+    data = [{'username':'admin'},{'username':'local'}]
     return jsonify(data)
 
 
@@ -25,19 +25,37 @@ def index():
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == "GET":
-        return  render_template('auth/login.html')
+        session.pop('error', default=None)
+        if session.get('username') != None  :
+            return redirect(url_for('index'))
+        return  render_template('auth/login.html',massage=None)
     elif request.method == "POST":
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+        data= request.form
+        if func.login.check_user(data) > 0:
+            session['username']  = request.form['username']
+            return redirect(url_for('index',massage=None))
+        return render_template('auth/login.html',massage ="email or password is valid!!")# redirect(url_for('index'))
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return  render_template('auth/register.html')
+    if request.method == "GET":
+        if session.get('username') != None  :
+            return redirect(url_for('index'))    
+        return  render_template('auth/register.html')
+    elif request.method == "POST":
+        data = request.form
+        if func.register.select_user(data) == 0:
+            r = func.register.insert_user(data)
+            session['error'] = {"error":"success","color":"green"}
+            return redirect(request.referrer)
+        session['error'] = {"error":"valid","color":"red"}
+        return redirect(request.referrer)
+
 
 @app.route('/logout',methods=['GET'])
 def logout():
-    session.pop('username', default=None)
+    session.pop('username',default=None)
     return  redirect(url_for('login'))
 
 if __name__  == "__main__":
--- function/func.py
+++ function/func.py
@@ -1,4 +1,43 @@
+import mysql.connector
+from datetime import datetime
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="dapp"
+)
 
-def fn(dic:list):
-    return dic 
+mydb.reconnect(attempts=1, delay=0)
+mycursor = mydb.cursor(dictionary=True)
+class register:
+    def insert_user(userobj:dict):
+        mydb.connect()
+        current_dateTime = datetime.now()
+        sql = "INSERT INTO users (`email`, `password`,`creat_date`,`updat_date`,`active`) VALUES (%s, %s, %s, %s, %s) ;"
+        val = (userobj['username'], userobj['password'],current_dateTime,current_dateTime,1)
+        r = mycursor.execute(sql, val)
+        mydb.commit()
+        mydb.close()
+        return r
+    def select_user(userobj:dict):
+        mydb.connect()
+        mycursor.execute("SELECT email, COUNT(*) FROM users WHERE email = %s GROUP BY email", (userobj['username'],))
+        # Add THIS LINE
+        results = mycursor.fetchall()
+        # gets the number of rows affected by the command executed
+        row_count = mycursor.rowcount
+        mydb.close()
+        return row_count
+
+class login:
+    def check_user(userobj:dict):
+        mydb.connect()
+        mycursor.execute("SELECT email, COUNT(*) FROM users WHERE email = %s and `password` = %s and active =1 GROUP BY email", (userobj['username'],userobj['password'],))
+        # Add THIS LINE
+        results = mycursor.fetchall()
+        # gets the number of rows affected by the command executed
+        row_count = mycursor.rowcount
+        mydb.close()
+        return row_count
+        
